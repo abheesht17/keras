a1e5ff86d75e179cb40357bb5b83606b72942c7b
Abheesht Sharma <abheesht@google.com>
2026-01-12 16:02:06 +0530

    Restore original

--- keras/src/backend/jax/trainer.py
+++ keras/src/backend/jax/trainer.py
@@ -1019,15 +1019,15 @@ class JAXEpochIterator(EpochIterator):
         layouts = None
         print(f"--->{self.data_adapter=}")
         for data in self.data_adapter.get_jax_iterator():
-            # if layouts is None:
-            #     layouts = tree.map_structure(
-            #         lambda d: distribution.get_data_layout(
-            #             d.shape
-            #         ).backend_layout,
-            #         data,
-            #     )
-            # yield _distribute_data(data, layouts)
-            yield data
+            if layouts is None:
+                layouts = tree.map_structure(
+                    lambda d: distribution.get_data_layout(
+                        d.shape
+                    ).backend_layout,
+                    data,
+                )
+            yield _distribute_data(data, layouts)
+            # yield data
 
     def _prefetch_numpy_iterator(self, numpy_iterator):
         """Shard and prefetch batches on device.
